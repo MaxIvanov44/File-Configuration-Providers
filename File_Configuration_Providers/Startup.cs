34bc9adbf9f9a090cd5a791cfcd1632061346ce1
MaxIvanov44 <Lvanovmaksim200033@gmail.com>
2020-01-23 18:40:52 +0300

    defconfig

--- File_Configuration_Providers/Startup.cs
+++ File_Configuration_Providers/Startup.cs
@@ -2,7 +2,9 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Collections.Generic;
 
 namespace File_Configuration_Providers
 {
@@ -10,23 +12,33 @@ namespace File_Configuration_Providers
     {
         private IConfiguration AppConfiguration { get; set; }
 
-        public Startup()
+        public Startup(IConfiguration config)
         {
             //var builder = new ConfigurationBuilder().AddJsonFile("conf.json");
             //var builder = new ConfigurationBuilder().AddXmlFile("config.xml");
-            var builder = new ConfigurationBuilder().AddIniFile("conf.ini");
+            var builder = new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string, string>
+                {
+                    {"name","TOM" },
+                    { "age","35"}
+                })
+                .AddConfiguration(config);
             AppConfiguration = builder.Build();
         }
-
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IConfiguration>(provider => AppConfiguration);
+        }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            var color = AppConfiguration["color"];
-            //var text = AppConfiguration["text:description:data"]; //для .xml
-            var text = AppConfiguration["text"];
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync($"<p style='color:{color};'>{text}</p>");
-            });
+            //var color = AppConfiguration["color"];
+            ////var text = AppConfiguration["text:description:data"]; //для .xml
+            //var text = AppConfiguration["firstname"];
+            app.UseMiddleware<ConfigMiddleware>();
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync($"<p style='color:{color};'>{text}</p>");
+            //});
         }
     }
 }
